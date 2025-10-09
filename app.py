74aa2af321c6cb25804e4571966f7c2990c43851
MatsunagaAtsuki <matsuatk@outlook.jp>
2025-10-09 14:05:42 +0900

    フォルダ追加
    フォルダ構成テキスト作成

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# 起動確認用のルート
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
