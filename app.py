d9f55d151629dae9c34f0bcc611514907a4ec046
syxnji <syunji0710@gmail.com>
2025-11-14 10:33:01 +0900

    /generateの追加

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from dotenv import load_dotenv
 import os
-from prefs_models import init_db
+from utils.db_con import init_db
+import services.ai.generateImg as generateImg
 
 app = Flask(__name__)
 
@@ -22,6 +23,14 @@ def health():
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/generate')
+def generate():
+    # 入力画像のパスを3つ指定
+    image_human = "images/input/male_model.png"
+    image_clothes_top = "images/input/clothes_c.png"
+    image_clothes_bottom = "images/input/clothes_d.png"
+    return generateImg.main(image_human, image_clothes_top, image_clothes_bottom)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
